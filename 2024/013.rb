4b3101542c010a39b4aa1227cfd67da19593b938
Ian Chesal <ian@withpersona.com>
2024-12-10 12:48:25 -0500

    Day 7: Part 1 working
    Had to switch to recursion in the end. Though it still runs pretty
    quickly. The stopping criteria took me forever to figure out.

--- 2024/013.rb
+++ 2024/013.rb
@@ -11,53 +11,24 @@ def continue_execution
   puts ''
 end
 
-def generate_operations(values)
-  # puts 'generate_operations()'
-  operations = []
-  i = 0
-  while i < values.length - 1
-    ops = Array.new(values.length - 1, '+')
-    j = 0
-    while j < i
-      ops[j] = '*'
-      j += 1
-    end
-    k = values.length - 1
-    while k >= j
-      ops[k] = '*' if k <= ops.length - 1
-      ops[k + 1] = '+' if k <= ops.length - 2
-      operations.append(ops.clone) if ops != operations[-1]
-      # pp operations
-      k -= 1
-    end
-    i += 1
-  end
+def subtract(target, array)
+  puts "subtract(#{target}, #{array})"
 
-  operations
+  val = array.pop
+  return (target - val) == array[0] if array.length == 1
+  return false if (target - val) < 0
+
+  subtract(target - val, array.clone) || divide(target - val, array.clone)
 end
 
-def compute(values, operations)
-  # puts 'compute()'
-  # pp values
-  # pp operations
-  total    = values.shift
-  operator = operations.shift
+def divide(target, array)
+  puts "divide(#{target}, #{array})"
 
-  while values.length.positive?
-    case operator
-    when '+'
-      total += values.shift
-    when '*'
-      total *= values.shift
-    else
-      raise StandardError("Unknown operation: #{operator}")
-    end
-    operator = operations.shift
-  end
+  val = array.pop
+  return (target / val) == array[0] if array.length == 1
+  return false if target % val != 0
 
-  # pp total
-
-  total
+  subtract(target / val, array.clone) || divide(target / val, array.clone)
 end
 
 File.readlines('input-007.txt', chomp: true).each do |line|
@@ -69,19 +40,8 @@ end
 
 inputs.each do |input|
   solution = input.shift
-  generate_operations(input).each do |ops|
-    computed = compute(input.clone, ops.clone)
-    next unless solution == computed
-
-    puts 'Found solution at: '
-    pp input
-    pp ops
-    pp solution
-    sum += solution
-    # continue_execution
-    break
-  end
-  puts "Sum: #{sum}"
+  puts "#{solution}: #{input}"
+  sum += solution if subtract(solution, input.clone) || divide(solution, input.clone)
 end
 
 puts ''
